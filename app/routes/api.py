76bf0b551261bdbbcd47c0aaa036dbaa4882f4f1
Utkarsh Mishra <iiutkarshmishraii@gmail.com>
2022-01-05 16:07:03 +0530

    added product api

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,3 +1,5 @@
+from app.database import connect_db
+from app.database.Item import Item
 from app.models import BecknResponse
 from flask import Blueprint, make_response, request
 
@@ -10,6 +12,42 @@ def search_for_product():
 
     response = make_response()  # Todo: convert ml response to becknresponse and return
     return response
+
+
+@api_route.get("/products")
+async def get_products():
+    await connect_db()
+    page = int(request.args.get("page") or 1)
+    LIMIT = 50
+    OFFSET = LIMIT * (page - 1) if page > 1 else 0
+
+    items = await Item.aggregate(
+        [
+            {"$skip": OFFSET},
+            {"$limit": LIMIT},
+            {
+                "$lookup": {
+                    "from": "Category",
+                    "localField": "category_id",
+                    "foreignField": "_id",
+                    "as": "category",
+                }
+            },
+            {"$unwind": {"path": "$category"}},
+            {
+                "$unset": [
+                    "category_id",
+                    "matched",
+                    "location_id",
+                    "recommended",
+                    "related",
+                    "parent_item_id",
+                ]
+            },
+            {"$set": {"category": "$category.descriptor.name"}},
+        ]
+    ).to_list()
+    return {"products": items}
 
 
 __all__ = ("api_route",)
